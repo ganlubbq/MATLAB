cf108f189f93a6185afca40f9002b643a20ef59f
genticuka <genti.cu@gmail.com>
2014-05-10 16:19:41 +0200

    central limit method 1

--- addnoise.m
+++ addnoise.m
@@ -9,8 +9,8 @@ X=zeros(1,length(x));
    % for unifrnd randoms in [0,1], mu = 0.5 and var = 1/12 */
    % adjust X so mu = 0 and var = 1 */
 	
-   X = X - N/2;                % set mean to 0
-   X = X * sqrt(1 / N);       % adjust variance to 1
+   X = X - 0/2;                % set mean to 0
+   X = X * sqrt(12 / N);       % adjust variance to 1
    y=x+X;
 
 
