ea7c6aea1a807ce4c871c2bd966034f502de7ca8
mwilczynski <matii92@gmail.com>
2014-05-10 15:52:36 +0200

    awgn

--- addnoise.m
+++ addnoise.m
@@ -1,5 +1,17 @@
-function y=addnoise(x)
+function y=addnoise(X)
 
-m=length(x);
-%lakjsdoiwnowvp
-y=x+randn(1,m);
+%m=length(x);
+%y=x+randn(1,m);
+
+N=1000;
+X=[];
+   for i = 1:N
+      U = unifrnd(0,1,1,10);
+      X = X + U;
+   end
+	
+   % for unifrnd randoms in [0,1], mu = 0.5 and var = 1/12 */
+   % adjust X so mu = 0 and var = 1 */
+	
+   X = X .- N/2;                % set mean to 0
+   X = X .* sqrt(12 / N);       % adjust variance to 1
