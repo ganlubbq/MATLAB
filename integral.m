bce2592569b410e988c31401257281837ff9c2b6
genticuka <genti.cu@gmail.com>
2014-05-24 14:18:39 +0200

    numerical integral normalized (dx=1)

--- integral.m
+++ integral.m
@@ -1,8 +1,11 @@
 function y=integral(x)
 y=0;
-for i=1:length(x)-1
-    y=y+((x(i)+x(i+1))/2);
+z=[x(1) x(2:length(x))+x(1:length(x)-1) x(length(x))];
+
+for i=1:length(z)
+    y=y+z(i);
     
 end
+y=y/2;
 end
     
