a6c47f5bf50e4cd7d9a2974cc596bacda43a10c2
genticuka <genti.cu@gmail.com>
2014-05-29 23:07:40 +0200

    add noise for specified snr

--- addnoise.m
+++ addnoise.m
@@ -4,10 +4,10 @@ Tb=0.01;
 Ts=0.0001;
 c=Tb/Ts;
 
-x=abs((sig(c:2*c))^2);
+x=abs((sig(c:2*c)).^2);
 sig_power=(1/2*Tb)*integral(x);
-noise_power=sig_power*(10^(10*snr));
-noise=noise_power*rand(1,length(sig));
+noise_power=sig_power/(10^(10*snr));
+noise=(noise_power/2)*rand(1,length(sig));
 y=sig+noise;
 end
 
