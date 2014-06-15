c4595e6fa7b6723f63b10aef91edc6dfe22c566c
genticuka <genti.cu@gmail.com>
2014-06-15 14:35:17 +0200

    split mod_test.m to have eye_diag separate, modified BER.m

--- BER.m
+++ BER.m
@@ -6,7 +6,7 @@ Tb=0.01;
 Ts=0.0001;
 c=Tb/Ts;
 
-N=100;
+N=1000;
 
 x=bitstr(N);
 X=zero2negone(x);
@@ -14,11 +14,11 @@ X=zero2negone(x);
 s=mod_test(X);
 nErr=[];
 y=[];
-Eb_N0_dB = [-10:10];
+Eb_N0_dB = [-20:20];
 for i=1:length(Eb_N0_dB)
     y=[];
-    y = addnoise(s,-Eb_N0_dB(i)); % additive white gaussian noise
-
+    y = addnoise(s,Eb_N0_dB(i)); % additive white gaussian noise
+    z=[];
    % receiver - hard decision decoding
    z=demodulate(y);
   
@@ -36,7 +36,7 @@ figure
 semilogy(Eb_N0_dB,theoryBer,'b.-');
 hold on
 semilogy(Eb_N0_dB,simBer,'mx-');
-axis([-10 10 10^-5 0.5])
+axis([-20 20 10^-5 0.5])
 grid on
 legend('theory', 'simulation');
 xlabel('Eb/No, dB');
--- addnoise.m
+++ addnoise.m
@@ -7,7 +7,7 @@ c=Tb/Ts;
 x=abs((sig(c:2*c)).^2);
 sig_power=(1/2*Tb)*integral(x);
 noise_power=sig_power/(10^(snr/10));
-noise=(noise_power/2)*rand(1,length(sig));
+noise=(noise_power/sqrt(1))*randn(1,length(sig));
 y=sig+noise;
 end
 
