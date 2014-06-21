c90464a686d34f2e2593912513803e09a5a73a79
mwilczynski <matii92@gmail.com>
2014-06-21 22:22:56 +0200

    gui - version without ber

--- eye_diag.m
+++ eye_diag.m
@@ -1,4 +1,5 @@
-function [s1,s2]=eye_diag(x)
+function s1=eye_diag(x)
+%function [s1,s2]=eye_diag(x)
 
 Tb=0.01;
 Ts=0.0001;
@@ -6,14 +7,16 @@ c=Tb/Ts;
 
 
 %x=bitstr(10);
-%y=zero2negone(x);
+%y=zero2negone(x); <----- to ma byc na wejsciu
 [a,b]=demux(x);
-
 [z,x]=halfsins(a,b);
 
 
 t=-Tb:Ts:((length(z)-1-c)*Ts);
 fc=2/Tb;
-s1=z.*cos(2*pi*fc*t);
-s2=x.*sin(2*pi*fc*t);
+s1=z.*cos(2*pi*fc*t); %plot s1 cos and -cos
+%s2=x.*sin(2*pi*fc*t); %plot s2 sin and -sin SPROBOWAC SUBPLOT W FUNKCJI
+
+%subplot(2,1,1), plot(t,s1,t,z,t,-z), grid on 
+%subplot(2,1,2), plot(t,s2,t,x,t,-x), grid on
 end
