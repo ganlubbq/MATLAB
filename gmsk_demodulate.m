c72b53d5a7a0ff67e6355046ceb672558f775ffb
genticuka <genti.cu@gmail.com>
2014-06-21 22:15:10 +0200

    gmsk demodulation

---
+++ gmsk_demodulate.m
@@ -0,0 +1,36 @@
+function z=gmsk_demodulate(x)
+
+global Tb Ts c
+
+%Tb=0.01;
+%Ts=0.0001;
+%c=Tb/Ts;
+fc=2/Tb;
+
+t=-Tb:Ts:((length(x)-1-c)*Ts);
+z=zeros(1,length(x)/c-1);
+ec=cos(2*pi*fc*t);
+even=x.*ec;
+oc=sin(2*pi*fc*t);
+odd=x.*oc;
+j=1;
+for i=1:2*c:length(even)-2*c
+    z(j)=integral(even(i:i+2*c))>0;
+    j=j+2;
+end
+j=2;
+for i=c:2*c:length(even)-c
+    z(j)=integral(odd(i+c:i+2*c))>0;
+    j=j+2;
+end
+%for i=1:length(z)
+ %   if z(i)>0
+  %      y(i)=1;
+ %   else
+   %     y(i)=-1;
+   % end
+end
+    
+    
+    
+
