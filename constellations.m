42b59bce0cfa53a75bcf326b57a5f2361a05262a
genticuka <genti.cu@gmail.com>
2014-06-22 17:39:33 +0200

    msk and gmsk constellations

--- constellations.m
+++ constellations.m
@@ -17,6 +17,19 @@ Ts=0.001;
 
 
 [E,O]=gmsk_filter(x);
-[gI,gO]=gmsk_carriers(E,O);
+t=-Tb:Ts:((length(E)-1-c)*Ts);
 
+ec=E.*cos(pi*t/(2*Tb)); 
+oc=O.*sin(pi*t/(2*Tb));
+[gI,gO]=gmsk_carriers(ec,oc);
+g=gI+gO;
+G=fft(g);
+M=fft(mI+mO);
+
+figure(1)
 plot(mI,mO,'bo',gI,gO,'rx-')
+figure(2)
+subplot(2,1,1)
+plot(abs(G(1000:3000)))
+subplot(2,1,2)
+plot(abs(M(1000:3000)))
